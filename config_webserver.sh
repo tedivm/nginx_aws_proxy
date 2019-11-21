3139fb55cb18e693eb154e467240671df7d757f8
mfergus1 <mia@radai.com>
2019-11-21 13:34:05 -0800

    Add host option:

--- config_webserver.sh
+++ config_webserver.sh
@@ -1,4 +1,10 @@
 #!/bin/env bash
+
+####################
+# Trust Private CA #
+####################
+
+
 echo "Pulling certificate for ${FQDN}"
 
 ## SSL CERTIFICATES
@@ -10,55 +16,66 @@ elif [[ -z "$FQDN" ]]; then
     exit 1
 fi
 
-arn_re=".*(arn:.*)\".*\"DomainName\": \"${FQDN}\".*"
 secret_re='.*"SecretString": "(.*)",.*'
-cert_re='.*"CertificateChain": "(.*-----).*"PrivateKey": "(.*-----).*'
-
-if [[ $(aws acm list-certificates) =~ $arn_re ]]; then
-  cert_arn=${BASH_REMATCH[1]}
-fi
+cert_arn=$(eval "aws acm list-certificates --query 'CertificateSummaryList[?DomainName==\`${FQDN}\`]'.[CertificateArn] --output text")
 
 if [[ $(aws secretsmanager get-secret-value --secret-id ${FQDN}) =~ $secret_re ]]; then
   cert_pass_phrase=${BASH_REMATCH[1]}
-  echo $cert_pass_phrase
 else
   echo "No cert pass phrase by the domain ${FQDN}"
   exit 1
 fi
 
-if [[ $(aws acm export-certificate --certificate-arn $cert_arn --passphrase $cert_pass_phrase) =~ $cert_re ]]; then
-
-  if [ ! -d "/etc/ssl/certs/" ]; then
-    mkdir "/etc/ssl/certs/"
-    if [ ! -f "/etc/ssl/certs/${FQDN}.crt"]; then
-      echo "Creating key file for certificate key"
-    fi
+# SSL Certificate
+if [ ! -d "/etc/ssl/certs/" ]; then
+  mkdir "/etc/ssl/certs/"
+  if [ ! -f "/etc/ssl/certs/${FQDN}.crt"]; then
+    echo "Creating key file for certificate key"
+    touch "/etc/ssl/certs/${FQDN}.crt"
   fi
-  if [[ "$(echo ${BASH_REMATCH[1]})"!="$(cat '/etc/ssl/certs/${FQDN}.crt')" ]]; then
-    echo "${BASH_REMATCH[1]}" > "/etc/ssl/certs/${FQDN}.crt"
-    echo "copying to crt file"
-    restart_nginx="true"
-  else
-  	echo "Certificate value has not changed, not updating."
-  fi
-
-  if [ ! -d "/etc/ssl/private/" ]; then
-    mkdir "/etc/ssl/private/"
-    if [ ! -f "/etc/ssl/private/${FQDN}.key"]; then
-      echo "Creating key file for certificate key"
-    fi
-  fi
-  if [[ "$(echo ${BASH_REMATCH[2]})"!="$(cat '/etc/ssl/private/${FQDN}.key')" ]]; then
-    echo "${BASH_REMATCH[2]}" > "/etc/ssl/private/${FQDN}.key"
-    "copying to key file"
-    restart_nginx="true"
-  else
-  	echo "Certificate value has not changed, not updating."
-  fi
-
 fi
 
-### NGINX
+ssl_cert=$(aws acm export-certificate --certificate-arn $cert_arn --passphrase $cert_pass_phrase | jq -r '"\(.Certificate)\(.CertificateChain)"')
+if [[ $ssl_cert!="$(cat /etc/ssl/certs/${FQDN}.crt)" ]]; then
+  echo "Copying ACM cert to file /etc/ssl/certs/${FQDN}.crt"
+  echo "$ssl_cert" > "/etc/ssl/certs/${FQDN}.crt"
+  restart_nginx="true"
+else
+  echo "Certificate value has not changed, not updating."
+fi
+
+# SSL Key
+if [ ! -d "/etc/ssl/private/" ]; then
+  mkdir "/etc/ssl/private/"
+  if [ ! -f "/etc/ssl/private/${FQDN}.key"]; then
+    echo "Creating key file for certificate key"
+    touch "/etc/ssl/private/${FQDN}.key"
+  fi
+fi
+ssl_key=$(aws acm export-certificate --certificate-arn $cert_arn --passphrase $cert_pass_phrase | jq -r '"\(.PrivateKey)"')
+if [[ $ssl_key!="$(cat /etc/ssl/private/${FQDN}.key)" ]]; then
+  echo "Copying ACM key to file /etc/ssl/private/${FQDN}.key"
+  echo "$ssl_key" > "/etc/ssl/private/${FQDN}.key"
+  restart_nginx="true"
+else
+  echo "Key value has not changed, not updating."
+fi
+
+####################
+# Trust Private CA #
+####################
+
+if [[ -v $PRIVATE_CA_NAME ]]; then
+  curl -s -o /usr/local/share/ca-certificates/${PRIVATE_CA_NAME}.crt ${PRIVATE_CA_URL} && \
+    chmod 644 /usr/local/share/ca-certificates/${PRIVATE_CA_NAME}.crt && \
+    update-ca-certificates
+fi
+
+
+###########
+### NGINX #
+###########
+
 # Restore original default config if no config has been provided
 if [[ ! "$(ls -A /etc/nginx/conf.d)" ]]; then
     cp -a /etc/nginx/.conf.d.orig/. /etc/nginx/conf.d 2>/dev/null
@@ -69,6 +86,11 @@ function ReplaceEnvironmentVariable() {
     grep -rl "\$ENV{\"$1\"}" $3|xargs -r \
         sed -i "s|\\\$ENV{\"$1\"}|$2|g"
 }
+
+
+if [ -z "$HOST" ]; then
+    export HOST="$host"
+fi
 
 if [ -n "$DEBUG" ]; then
     echo "Environment variables:"
@@ -84,9 +106,10 @@ for _curVar in `env | awk -F = '{print $1}'`;do
 done
 
 # Run nginx
-exec /usr/sbin/nginx start
+echo $cert_pass_phrase | service nginx start
 
 if [ $restart_nginx=="true" ]; then
-  exec /usr/sbin/nginx stop
-  exec /usr/sbin/nginx start
+  echo "Restarting nginx per certificate change"
+  service nginx stop
+  echo $cert_pass_phrase | service nginx start
 fi
